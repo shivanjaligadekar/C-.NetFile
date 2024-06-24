10ac1e61a2eabe8848f74c834be4447a19927253
Shivanjali Gadekar <145742159+shivanjaligadekar@users.noreply.github.com>
2024-06-24 09:25:01 +0530

    Add files via upload

---
+++ AsyncawaitDemo/AsyncawaitDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Print1to10();
+        PrintHello();
+        Task<int> ans = multiplyNos(2, 3);
+        Console.WriteLine("Answer="+ans.Result);
+        Console.WriteLine("hello");
+    }
+    public static async void Print1to10()
+    {
+        await Task.Run(() =>
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Task.Delay(10).Wait();   
+
+            }
+        });
+    }
+    public static async void PrintHello()
+    {
+        await Task.Run(() => { 
+            for (int i = 1;i <= 1;i++)
+            {
+                Console.WriteLine("Hello");
+                Task.Delay(10).Wait();
+
+            }
+        });
+    }
+    public static async Task<int> multiplyNos(int i,int j)
+    {
+        int ans = 0;
+        await Task.Run(() => {
+            for(int i = 1; i<10; i++) 
+            {
+                ans = i * j * 10;
+            }
+            
+        });
+        return ans;
+    }
+}
